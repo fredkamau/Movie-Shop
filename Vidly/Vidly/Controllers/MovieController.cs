68224331be447af3a9a3e9383055a4a78eb3769d
fredkamau <fredrickamau18@gmail.com>
2019-07-10 18:11:14 +0300

    added customer, movie and rental model

---
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            Movie m1 = new Movie { Id=1, Name="Titanic"};
+            var customer = new List<Customer>
+            {
+                new Customer { Name = "Fred" },
+                new Customer { Name = "Kyle" }
+            };
+            MovieCustomerViewModel mcv = new MovieCustomerViewModel
+            {
+                Customers = customer,
+                Movies = m1
+            };
+            return View(mcv);
+        }
+    }
+}
---
+++ Vidly/Vidly/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        //Navigation Property
+        public MembershipType MembershipType { get; set; }
+        //we dont need the entire membershiptype but the id as the FK
+        public int MembershipTypeId { get; set; }
+
+    }
+}
