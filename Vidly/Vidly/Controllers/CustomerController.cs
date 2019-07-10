931f8757ad45ebb0b236c7685c95cefe40a0af06
fredkamau <fredrickamau18@gmail.com>
2019-07-10 23:25:27 +0300

    index and details pages created for customer Model

--- Vidly/Vidly/Controllers/CustomerController.cs
+++ Vidly/Vidly/Controllers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
@@ -20,11 +21,21 @@ namespace Vidly.Controllers
         }
 
         // GET: Customer
-        public ActionResult Index()
+
+        public ViewResult Index()
         {
-            var customers = _context.Customers.ToList();
-            return View();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
         }
-       
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(m => m.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
     }
 }
