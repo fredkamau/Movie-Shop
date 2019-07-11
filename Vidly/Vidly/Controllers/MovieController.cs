04db6101613a089dc62af15509cea531cd09ae65
fredkamau <fredrickamau18@gmail.com>
2019-07-11 12:12:53 +0300

    AddedMovieDetailsPage

--- Vidly/Vidly/Controllers/MovieController.cs
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -5,15 +5,36 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModel;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
+        private ApplicationDbContext _MovieContext;
+        public MovieController()
+        {
+            _MovieContext = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _MovieContext.Dispose();    
+        }
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            var movies = _MovieContext.Movies.Include(c => c.Genre).ToList();
+            return View(movies);
+        }
+        public ActionResult Details(int id)
+        {
+            var movie = _MovieContext.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie); 
+
         }
     }
 }
